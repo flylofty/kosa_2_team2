9900bae6c44f40928035f8a69731ab992fe37373
Youngnam Lee <lyn00227@naver.com>
2022-10-03 17:41:30 +0900

    로또1 복습

---
+++ src/lotto1/Lotto1Main.java
@@ -0,0 +1,24 @@
+package lotto1;
+
+public class Lotto1Main {
+
+    // 정수 배열을 사용
+    static int[] lottoNumber = new int[6];
+
+    public static void main(String[] args) {
+        // 아래의 순서로 기능 배치
+        // 로또 번호 배열에 넣기 createLottoNumber
+        // 버블 정렬 sortLottoNumber
+        // 결과 출력 printLottoNumber
+    }
+
+    // 실제 구현 메서드 내용 채우기
+    private static void printLottoNumber() {
+    }
+
+    private static void sortLottoNumber() {
+    }
+
+    private static void createLottoNumber() {
+    }
+}
